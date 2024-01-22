beb46f889032c8e0ddfc7e1b93458e3a9ea8ffe1
Anton Padvalnikau <podvalnikov0@gmail.com>
2024-01-22 20:18:18 +0300

    Add task to push data in kafka

--- app/main.py
+++ app/main.py
@@ -2,22 +2,17 @@ from fastapi import FastAPI
 import asyncio
 from app.websockets import run_websocket
 from app.consumer import consume
+from app.config import Config
 
 app = FastAPI()
 
 
 @app.on_event("startup")
 async def startup_event():
-    kafka_task = asyncio.create_task(consume())
     ws_task = asyncio.create_task(run_websocket())
+    kafka_task = asyncio.create_task(consume())
 
 
 @app.get("/")
 async def read_root():
     return {"message": "Hello World"}
-
-
-@app.get("/ws/upd")
-async def upd():
-    global assets
-    assets = []
